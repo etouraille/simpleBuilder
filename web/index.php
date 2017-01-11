a6b381af916c8318347cd7ef7f11026b3989ed92
etouraille <Edouard Touraille>
2017-01-11 19:54:12 +0100

    [fix]

--- web/index.php
+++ web/index.php
@@ -2,13 +2,16 @@
 
 $postedRawData = file_get_contents('php://input');
 
-$payload = json_decode( $postedRawData , false);
+$payload = json_decode( $postedRawData , true );
 
-$ref = $paylod['ref'];
+
+$ref = $payload['ref'];
 
 if('refs/head/DEV' == $ref ) {
 
-	$fp = fopen(sprintf('/src/push-time/%s',date('m-d-y H:i:s')),'w+');
+	var_dump(is_dir('/src/push-time'));
+
+	$fp = fopen(sprintf('/src/push-time/%s',date('m-d-y.H:i:s')),'w+');
 	fwrite($fp, 'push on branch dev');
 	fclose($fp);
 
