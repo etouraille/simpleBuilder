2a9b8e8139eb8fe3b24df59febf4a377165ca8d4
etouraille <Edouard Touraille>
2017-01-13 10:04:04 +0100

    [sysadmin] add password stuff : to deseaper

--- web/index.php
+++ web/index.php
@@ -6,12 +6,14 @@ $payload = json_decode( $postedRawData , true );
 
 $ref = $payload['ref'];
 
-$password = 'toto';
+$password = file_exist('/src/config/password')?file_get_contents('/src/config/password'):'anonymous';
+// to deaseaper : the deploy script shouldn't be launch in sudo
 $host_ip = file_get_contents('/src/config/host_ip');
+
 
 if('refs/heads/DEV' == $ref ) {
 
-	$cd = sprintf("ssh etouraille@%s echo '%s'|sudo -S /home/etouraille/kat --env staging", $host_ip, $password);
+	$cd = sprintf("ssh etouraille@%s echo '%s'|sudo -S /home/etouraille/kat --env staging > /var/log/install.log", $host_ip, $password );
 	exec($cd);
 
 }
