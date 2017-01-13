7f02ea37e8321450e6f9cad2972d84facb205a74
etouraille <Edouard Touraille>
2017-01-13 13:05:27 +0100

    [wip] no password , and cleaning

--- web/index.php
+++ web/index.php
@@ -6,14 +6,11 @@ $payload = json_decode( $postedRawData , true );
 
 $ref = $payload['ref'];
 
-$password = file_exist('/src/config/password')?file_get_contents('/src/config/password'):'anonymous';
-// to deaseaper : the deploy script shouldn't be launch in sudo
 $host_ip = file_get_contents('/src/config/host_ip');
-
 
 if('refs/heads/DEV' == $ref ) {
 
-	$cd = sprintf("ssh etouraille@%s echo '%s'|sudo -S /home/etouraille/kat --env staging > /var/log/install.log", $host_ip, $password );
+	$cd = sprintf("ssh etouraille@%s /home/etouraille/kat --env staging --sudoer true > /var/log/install.log", $host_ip, $password );
 	exec($cd);
 
 }
